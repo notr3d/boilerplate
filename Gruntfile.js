76693cff6d2c2722f0f0a469aa7f7695835af8c3
Александр Некрасов <lxmrrr@gmail.com>
2016-02-18 11:52:02 +0300

    min файлы заменены на обычные, исправлен gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,24 @@
 'use strict';
  
 module.exports = function (grunt) {
-    // load all grunt tasks
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    
+    grunt.loadNpmTasks('grunt-contrib-watch');    
     grunt.initConfig({
         less: {
-            // production config is also available
             development: {
                 options: {
-                    // Specifies directories to scan for @import directives when parsing. 
-                    // Default value is the directory of the source, which is probably what you want.
                     paths: ["less/"],
                 },
                 files: {
-                    // compilation.css  :  source.less
                     "css/style.css": "less/style.less"
                 }
             },
         },
-      
-        postcss: {
-            options: {
-                map: false,
-                processors: [
-                    require('autoprefixer')({
-                        browsers: ['last 2 versions']
-                    })
-                ]
-            },
-            dist: {
-                src: 'css/*.css'
-            }
-        },
-      
         watch: {
-            // if any .less file changes in directory "public/css/" run the "less"-task.
             files: "less/*.less",
             tasks: ["less"]
         },
-        // "less"-task configuration
     });
-     // the default task (running "grunt" in console) is "watch"
      grunt.registerTask('default', ['less:less']);
-     grunt.registerTask('default', ['postcss:css']);
      grunt.registerTask('default', ['watch']);
 };
